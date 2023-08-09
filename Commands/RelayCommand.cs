b1efe82540e9ab0b99005fbe21bd0de99f463ad1
Joshua Turner <joshua.turner@bpsoftware.net>
2023-08-09 11:57:59 +1000

    Add project files.

---
+++ Commands/RelayCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WinUI3Demo.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private Action<object> _Execute { get; set; }
+        private Predicate<object> _CanExecute { get; set; }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _CanExecute(parameter);
+
+        public void Execute(object? parameter) => _Execute(parameter);
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinUI3Demo.Models
+{
+    public class Student
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public int LastName { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female,
+        Other
+    }
+}
